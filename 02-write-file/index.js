9f79246f7ac4d40603d9056c1868168fa0ba94c8
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-05-08 21:53:48 +0300

    refactor: rename const readLineProject

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,23 +5,23 @@ const file = path.join(__dirname, 'text.txt');
 
 let stream = fs.createWriteStream(file);
 
-const rl = readline.createInterface({
+const readLineProject = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     prompt: `Введите текст и нажмите Enter:\n`
 });
 
-rl.prompt();
-rl.on('line', (input) => {
+readLineProject.prompt();
+readLineProject.on('line', (input) => {
   input = `${input}\n`;
 
   if(input.trim() === 'exit') {
-    rl.close();
+    readLineProject.close();
   } else {
     stream.write(input);
   }
 });
 
-rl.on('close', () => {
+readLineProject.on('close', () => {
   console.log(`\nBuy!`);
 });
